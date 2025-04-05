acf22dd2edd7743b80a760b85d9c3deee4f7c989
Arpittgaikwad29 <158511811+Arpittgaikwad29@users.noreply.github.com>
2025-04-06 03:02:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,11 @@
 # config.py
 GEMINI_API_KEY = "AIzaSyBFJi_RhIbNND7FvvqYE28uEkpPqO00Lys"
+
+DB_CONFIG = {
+    "host": "trolley.proxy.rlwy.net",
+    "port": 32851,
+    "user": "root",
+    "password": "FGJzjbMcvDYXDFgOslZDJsHjghNTRIql",
+    "database": "railway"
+}
+
